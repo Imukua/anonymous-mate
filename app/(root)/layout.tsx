a73695e6a6ae595fb47ba5a8c5c658123e46fcf8
Imukua <ianmukuaa@gmail.com>
2024-03-18 15:50:21 +0300

    Moved: root group to app dir

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -15,7 +15,6 @@ export default function RootLayout({ children, }: {
     return (
         <html lang="en">
             <body className={`{$inter.className} bg-dark-1`}>
-
             </body>
         </html>
     );

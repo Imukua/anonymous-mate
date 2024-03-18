ddbe6f965a53a7addb7d5cc312ac706cc2e619d6
Imukua <ianmukuaa@gmail.com>
2024-03-18 18:28:35 +0300

    added: file for constants

---
+++ constants/index.js
@@ -0,0 +1,44 @@
+export const sidebardata = [
+    {
+      imgURL: "/assets/home.svg",
+      route: "/",
+      label: "Home",
+    },
+    {
+      imgURL: "/assets/search.svg",
+      route: "/search",
+      label: "Search",
+    },
+    {
+      imgURL: "/assets/activity.svg",
+      route: "/activity",
+      label: "Activity",
+    },
+    {
+      imgURL: "/assets/create.svg",
+      route: "/create-post",
+      label: "Create Post",
+    },
+    {
+      imgURL: "/assets/group.svg",
+      route: "/groups",
+      label: "groups",
+    },
+    {
+      imgURL: "/assets/profile.svg",
+      route: "/profile",
+      label: "Profile",
+    },
+  ];
+  
+  export const profileTabs = [
+    { value: "posts", label: "Posts", icon: "/assets/reply.svg" },
+    { value: "replies", label: "Replies", icon: "/assets/members.svg" },
+    { value: "tagged", label: "Tagged", icon: "/assets/tag.svg" },
+  ];
+  
+  export const groupTabs = [
+    { value: "posts", label: "Posts", icon: "/assets/reply.svg" },
+    { value: "members", label: "Members", icon: "/assets/members.svg" },
+    { value: "requests", label: "Requests", icon: "/assets/request.svg" },
+  ];

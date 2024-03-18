422d8a73d56e72d1a5ded752a28e81cb668509d0
Imukua <ianmukuaa@gmail.com>
2024-03-18 17:31:44 +0300

    added: TopBar component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,21 +1,28 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
-import React from "react";
+import TopBar from "@/components/shared/TobBar";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "Anonymate",
-    description: "A mental health support platform"
+  title: "Anonymate",
+  description: "A mental health support platform"
 }
 
-const inter = Inter({ subsets: ["latin"] })
-
-export default function RootLayout({ children, }: {
-    children: React.ReactNode
-}) {
-    return (
-        <html lang="en">
-            <body className={`{$inter.className} bg-dark-1`}>
-            </body>
-        </html>
-    );
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          <TopBar />
+          {children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }

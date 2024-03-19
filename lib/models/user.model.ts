75f1733f6396027fb98845562f004e873ee99412
Imukua <ianmukuaa@gmail.com>
2024-03-19 14:06:07 +0300

    added: mongoose user model

---
+++ lib/models/user.model.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schcema({
+    id: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    picture: String,
+    bio: String,
+    onboarded: {
+        type: Boolean,
+        default: false
+    },
+    lastLogin: {
+        type: Date,
+        default: Date.now
+    },
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post"
+        },
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    supportGroups: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "SupportGroup"
+        },
+    ]
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;

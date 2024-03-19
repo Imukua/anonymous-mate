dcca7eed5a260c2a62d0605aac0922a90729d514
Imukua <ianmukuaa@gmail.com>
2024-03-19 19:27:37 +0300

    added: user profile page boilerplate

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,32 @@
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+
+
+async function Page({ params }: { params: { id: string } }) {
+    const user = await currentUser();
+    if (!user) return null;
+
+
+    const userInfo = await fetchUser(params.id);
+    if (!userInfo?.onboarded) redirect("/onboarding");
+
+    return (
+        <section>
+            {/*Profile header component*/}
+
+            <div>
+
+            </div>
+
+        </section>
+    )
+
+
+
+}
+
+export default Page;

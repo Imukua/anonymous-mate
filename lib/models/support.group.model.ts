2046d3482e944c83e19570881a4447214a71ee1a
Imukua <ianmukuaa@gmail.com>
2024-03-19 14:05:56 +0300

    added: support group mongoose model

---
+++ lib/models/support.group.model.ts
@@ -0,0 +1,52 @@
+import { MongoTopologyClosedError } from "mongodb";
+import mongoose from "mongoose";
+
+const supportGroupSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    bio: String,
+    picture: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    founder: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    members: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    admins: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Posts"
+        },
+    ]
+});
+
+const SupportGroup =
+    mongoose.models.SupportGroup ||
+    mongoose.model("SupportGroup", supportGroupSchema);
+
+export default SupportGroup;

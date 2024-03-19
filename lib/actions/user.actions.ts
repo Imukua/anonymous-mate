94cea0f984c1a4dd6784ef743f917500ab26e00b
Imukua <ianmukuaa@gmail.com>
2024-03-19 20:59:34 +0300

    Added: fetch user posts function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -74,3 +74,35 @@ export async function fetchMembership(authUserId: string, groupId: string) {
         console.log("Error fetching membership:", error);
     }
 }
+
+export async function fetchUserPosts(userId: string) {
+    try {
+        connectTodb();
+
+        // Find all Posts authored by the user with the given userId
+        const posts = await User.findOne({ id: userId }).populate({
+            path: "posts",
+            model: Post,
+            populate: [
+                {
+                    path: "supportGroup",
+                    model: SupportGroup,
+                    select: "name id picture _id", // Select the "name" and "_id" fields from the "group" model
+                },
+                {
+                    path: "children",
+                    model: Post,
+                    populate: {
+                        path: "author",
+                        model: User,
+                        select: "name picture id", // Select the "name" and "_id" fields from the "User" model
+                    },
+                },
+            ],
+        });
+        return posts;
+    } catch (error) {
+        console.error("Error fetching user posts:", error);
+        throw error;
+    }
+}

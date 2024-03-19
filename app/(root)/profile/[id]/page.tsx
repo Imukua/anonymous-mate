5c22369a998daf990934a5d8aa187a21028ce808
Imukua <ianmukuaa@gmail.com>
2024-03-19 20:55:04 +0300

    Added: profile view page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,8 +1,33 @@
-function Page() {
-    return (
-        <></>
-    )
-}
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
 
+
+async function Page({ params }: { params: { id: string } }) {
+    const user = await currentUser();
+    if (!user) return null;
+
+
+    const userInfo = await fetchUser(params.id);
+    if (!userInfo?.onboarded) redirect("/onboarding");
+
+    return (
+        <section>
+            {/*Profile header component*/}
+
+            <div className="mt-9">
+
+
+            </div>
+
+        </section>
+    )
+
+
+
+}
 
 export default Page;

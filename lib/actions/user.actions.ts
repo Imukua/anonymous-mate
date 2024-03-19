e6c870df8594d2fe736605f6f35c3a983bfc92fd
Imukua <ianmukuaa@gmail.com>
2024-03-19 14:42:58 +0300

    added: function to onboard user to db

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,3 +1,4 @@
+import { revalidatePath } from "next/cache";
 import SupportGroup from "../models/support.group.model";
 import User from "../models/user.model";
 import { connectTodb } from "../mongoDB";
@@ -16,5 +17,44 @@ export async function fetchUser(userId: string) {
     } catch (error: any) {
         throw new Error(`failed to fetch user: ${error.message}`)
 
+    }
+}
+
+
+interface userProps {
+    userId: string;
+    name: string;
+    username: string;
+    bio: string;
+    picture: string;
+    path: string;
+}
+export async function updateUser({
+    userId,
+    name,
+    username,
+    bio,
+    picture,
+    path
+}: userProps): Promise<void> {
+    try {
+        connectTodb();
+        await User.findOneAndUpdate(
+            { id: userId },
+            {
+                username: username.toLowerCase(),
+                name,
+                bio,
+                picture,
+                onboarded: true
+            },
+            { upsert: true }
+        );
+
+        if (path === "/profile/edit") {
+            revalidatePath(path);
+        }
+    } catch (error: any) {
+        throw new Error(`failed to create/update user : ${error.message}`);
     }
 }

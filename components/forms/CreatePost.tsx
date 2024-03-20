465a784c6164a48bf872420131ecff4b4461e1b8
Imukua <ianmukuaa@gmail.com>
2024-03-20 15:23:25 +0300

    Added: create post form component

---
+++ components/forms/CreatePost.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usePathname, useRouter } from "next/navigation";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { postValidation } from "@/lib/validations/post";
+import { createPost } from "@/lib/actions/post.actions";
+
+
+function CreatePost({ userId }: { userId: string }) {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const form = useForm({
+        resolver: zodResolver(postValidation),
+        defaultValues: {
+            post: '',
+            accountId: userId,
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof postValidation>) => {
+        await createPost({
+            content: values.post,
+            author: userId,
+            groupId: null,
+            path: pathname,
+        });
+
+        router.push("/");
+    };
+
+    return (
+        <Form {...form}>
+            <form
+                className='mt-10 flex flex-col justify-start gap-10'
+                onSubmit={form.handleSubmit(onSubmit)}
+            >
+                <FormField
+                    control={form.control}
+                    name='post'
+                    render={({ field }) => (
+                        <FormItem className='flex w-full flex-col gap-3'>
+                            <FormLabel className='text-base-semibold text-light-2'>
+                                Thoughts
+                            </FormLabel>
+                            <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
+                                <Textarea rows={10} {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <Button type='submit' className='bg-primary-500'>
+                    Post Thoughts
+                </Button>
+            </form>
+        </Form>
+    );
+}
+
+export default CreatePost;

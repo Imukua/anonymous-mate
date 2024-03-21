66710900c6de85bf2baa8265cc7f4023be96e150
Imukua <ianmukuaa@gmail.com>
2024-03-21 13:26:55 +0300

    Implemented: Group profile view page

---
+++ app/(root)/groups/[id]/page.tsx
@@ -0,0 +1,69 @@
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { currentUser } from "@clerk/nextjs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { groupTabs } from "@/constants";
+import Image from "next/image";
+import PostsTab from "@/components/shared/PostsTab";
+import { fetchGroupInfo } from "@/lib/actions/supportGroup.actions";
+
+async function Page({ params }: { params: { id: string } }) {
+
+    const user = await currentUser();
+    if (!user) return null;
+
+    const groupInfo = await fetchGroupInfo(params.id);
+
+
+    return (
+        <section>
+            <ProfileHeader
+                accountId={groupInfo.id}
+                authUserId={user.id}
+                name={groupInfo.name}
+                username={groupInfo.username}
+                imgUrl={groupInfo.picture}
+                bio={groupInfo.bio}
+                type="supportGroup"
+            /><div className='mt-9'>
+                <Tabs defaultValue='posts' className='w-full'>
+                    <TabsList className='tab'>
+                        {groupTabs.map((tab) => (
+                            <TabsTrigger key={tab.label} value={tab.value} className='tab'>
+                                <Image
+                                    src={tab.icon}
+                                    alt={tab.label}
+                                    width={24}
+                                    height={24}
+                                    className='object-contain'
+                                />
+                                <p className='max-sm:hidden'>{tab.label}</p>
+
+                                {tab.label === "Posts" && (
+                                    <p className='ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2'>
+                                        {groupInfo.posts?.length}
+                                    </p>
+                                )}
+                            </TabsTrigger>
+                        ))}
+                    </TabsList>
+                    {groupTabs.map((tab) => (
+                        <TabsContent
+                            key={`content-${tab.label}`}
+                            value={tab.value}
+                            className='w-full text-light-1'
+                        >
+                            {/* @ts-ignore */}
+                            <PostsTab
+                                currentUserId={user.id}
+                                accountId={groupInfo._id}
+                                accountType='SupportGroup'
+                            />
+                        </TabsContent>
+                    ))}
+                </Tabs>
+            </div>
+        </section>
+    )
+}
+
+export default Page;

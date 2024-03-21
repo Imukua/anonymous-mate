710f28d769235af996f8eb0fc0d3432525a5f409
Imukua <ianmukuaa@gmail.com>
2024-03-21 16:29:36 +0300

    Reduced: margiin top

--- components/shared/TobBar.tsx
+++ components/shared/TobBar.tsx
@@ -4,7 +4,7 @@ import { SignOutButton, SignedIn, UserButton } from "@clerk/nextjs";
 
 function TopBar() {
     return (
-        <nav className="fixed top-0 z-30 flex w-full items-center justify-between rounded-t-3xl bg-glassmorphism p-4 backdrop-blur-lg px-6 py-3 ">
+        <nav className="fixed top-0 z-30 flex w-full items-center justify-between rounded-t-3xl bg-glassmorphism  backdrop-blur-lg px-6 py-3 ">
             <Link href="/" className=" flex items-center gap-2">
                 <Image src="/assets/next.svg" alt="logo" width={28} height={28} />
                 <p className="text-sky-400">Anonymate</p>

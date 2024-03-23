ecc765fbd2cb41884f573a5c23cc06ff47d8ceb9
Imukua <ianmukuaa@gmail.com>
2024-03-23 16:22:28 +0300

    Refactoed: create-post/page.tsx to read url params

--- app/(root)/create-post/page.tsx
+++ app/(root)/create-post/page.tsx
@@ -4,21 +4,26 @@ import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 
-async function Page({
-    groupId
-}: { groupId: string }) {
+async function Page(
+    { searchParams, }: { searchParams: { [key: string]: string | undefined } }) {
     const user = await currentUser();
-
     if (!user) return null;
+    console.log(searchParams?.gid);
 
 
     const userInfo = await fetchUser(user.id);
     if (!userInfo?.onboarded) redirect("/onboarding");
 
+
+
+
     return (
         <>
-            <h1 className="head-text">Create post </h1>
-            <CreatePost userId={userInfo._id} />
+            <h1 className="head-text">Create {searchParams?.gid ? "group" : "no"} post </h1>
+            <CreatePost
+                userId={userInfo._id}
+                groupId={searchParams?.gid || null}
+            />
         </>
     );
 }
